251a8c6de700c7a74393d18df12d88c9df4c18e1
takahasi <gaoqiaotaiyang237@gmail.com>
2024-10-26 17:11:23 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const getFormattedDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; 
+  const day = date.getDate();
+
+  return `${year}年${month}月${day}日`;
+}
+
+console.log(getFormattedDate());
